4d0499acb81b54139a65cd4f1ebf2947ed9cb9a9
21thecreative <77678156+21thecreative@users.noreply.github.com>
2021-01-20 20:44:55 +0900

    Create string.dart

---
+++ string.dart
@@ -0,0 +1,11 @@
+main(){
+//Triple quotes define a multi-line string.
+  print('''triple quoted strings
+are for multiple lines'''); 
+  // '가 세 개면 enter를 반영
+  
+// Dart supports sting interpolation.
+  var pi = 3.14;
+  print('pi is $pi');
+  print('tau is ${2*pi}');
+}
