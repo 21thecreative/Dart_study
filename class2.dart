d88a8310c0b631368e8858a9742aab6db338170e
21thecreative <77678156+21thecreative@users.noreply.github.com>
2021-01-20 22:06:39 +0900

    Update class2.dart

--- class2.dart
+++ class2.dart
@@ -3,24 +3,21 @@
 // Constructors는 form(주문서)를 통해 유저가 Car를 주문함. 결과값으로 나온 Car는 instance라고 부름.
 // instance를 가지고 차량의 method를 사용하는 것임.
 
-void main(){
-​
+void main(){ // 주문서를 통해 주문한 사람한테 보내는 과정
   Car myNewCar = new Car(4, 4, "pink", 5000);
   
   print(myNewCar.color);
   print(myNewCar.seat);
   print(myNewCar.door);
-​
   print(myNewCar.currentSpeed(50));
 }
-​
 class Car {
   int seat = 2;
   int door = 2;
-  String color = "white";
-  int engineSize = 1000;
+  String color = "white"; // 여기까지는 variable(변수) 설정값 지정
+  int engineSize = 1000; // 차량에 method(기능)을 주기 위함
   
-  Car(int fseat, int fdoor, String fcolor, int fengineSize){
+  Car(int fseat, int fdoor, String fcolor, int fengineSize){ // 주문서를 내린 것임(constructors)
     this.seat = fseat;
     this.door = fdoor;
     this.color = fcolor;
@@ -30,5 +27,4 @@ class Car {
  int currentSpeed(int accel){
    return accel * engineSize;
  }
-  
 }
